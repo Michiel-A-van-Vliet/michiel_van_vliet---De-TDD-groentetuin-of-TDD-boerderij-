6808b90a303bafb86389627af826ff75f5dcc69d
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-04-29 22:16:28 +0200

    Tweede functie gedaan: getYieldForCrop

--- farm.js
+++ farm.js
@@ -4,12 +4,25 @@ function getYieldForPlant(plant) {
   return plant.yield;
 }
 
-function getYieldForCrop(params) {
-  // to be created
+function getYieldForCrop(input) {
+  return input.crop.yield * input.numCrops;
 }
 
-function getTotalYield(params) {
-  // to be created
+function getTotalYield(crops) {
+  console.log(crops);
 }
 
 module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield };
+
+// const corn = {
+//     name: "corn",
+//     yield: 3,
+//   };
+//   const pumpkin = {
+//     name: "pumpkin",
+//     yield: 4,
+//   };
+//   const crops = [
+//     { crop: corn, numCrops: 5 },
+//     { crop: pumpkin, numCrops: 2 },
+//   ];
