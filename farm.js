4a140b40ebe2ccb356ec9cff46322c6e9c1331d7
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-04-29 21:59:10 +0200

    eerste versie, nog niks gedaan

---
+++ farm.js
@@ -0,0 +1,15 @@
+// module.exports = { func1, func2 }
+
+function getYieldForPlant(params) {
+  // to be created
+}
+
+function getYieldForCrop(params) {
+  // to be created
+}
+
+function getTotalYield(params) {
+  // to be created
+}
+
+module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield };
